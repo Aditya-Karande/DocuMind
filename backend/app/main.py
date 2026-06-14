3c2a3079a26ffa2bce91495a788b23b14f193099
Aditya-Karande <karandeaditya31@gmail.com>
2026-06-14 12:44:08 +0530

    Add document routes for deployment test

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 # from app.api.upload_route import upload_router
 # from app.api.query_route import query_router
 from app.api.chat_route import chat_router
-# from app.api.document_route import doc_router
+from app.api.document_route import doc_router
 from app.api.user_route import user_router
 from app.api.authentication import auth_router
 from fastapi.middleware.cors import CORSMiddleware
@@ -25,5 +25,5 @@ app.include_router(auth_router)
 # app.include_router(upload_router)
 # app.include_router(query_router)
 app.include_router(chat_router)
-# app.include_router(doc_router)
+app.include_router(doc_router)
 app.include_router(user_router)

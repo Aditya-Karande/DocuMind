9b46599114a97863ec07e19c16ea68f735febd43
Aditya-Karande <karandeaditya31@gmail.com>
2026-06-17 23:34:51 +0530

    remove pdf2image

--- backend/app/services/pdf_ocr_loader.py
+++ backend/app/services/pdf_ocr_loader.py
@@ -1,27 +1,47 @@
-from pdf2image import convert_from_path
+import fitz
 import base64
 import io
-from groq import Groq
-from langchain_core.documents import Document
 import os
+
+
+from groq import Groq
 from dotenv import load_dotenv
 from pathlib import Path
-
-
+from PIL import Image
+from langchain_core.documents import Document
 
 load_dotenv(Path(__file__).resolve().parents[2] / ".env")
-client = Groq(api_key=os.getenv("GROQ_API_KEY") or os.getenv("API_KEY"))
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY") or os.getenv("API_KEY")
+)
+
 
 def image_to_base64(image):
     buffer = io.BytesIO()
     image.save(buffer, format="PNG")
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
+
 def load_scanned_pdf(file_path):
-    images = convert_from_path(str(file_path))
+
+    pdf = fitz.open(str(file_path))
     extracted_text = ""
 
-    for image in images:
+    for page_num in range(len(pdf)):
+
+        page = pdf.load_page(page_num)
+
+        pix = page.get_pixmap(
+            matrix=fitz.Matrix(2, 2)
+        )
+
+        image = Image.frombytes(
+            "RGB",
+            [pix.width, pix.height],
+            pix.samples
+        )
+
         base64_image = image_to_base64(image)
 
         response = client.chat.completions.create(
@@ -38,19 +58,29 @@ def load_scanned_pdf(file_path):
                         },
                         {
                             "type": "text",
-                            "text": "Extract all text from this image exactly as it appears. Return only the extracted text, nothing else."
+                            "text": """
+Extract all text from this image exactly as it appears.
+Return only the extracted text.
+"""
                         }
                     ]
                 }
             ],
-            max_tokens=2000
+            max_tokens=4000
         )
 
-        extracted_text += response.choices[0].message.content + "\n"
+        extracted_text += (
+            f"\n\n--- PAGE {page_num + 1} ---\n\n"
+            + response.choices[0].message.content
+        )
+
+    pdf.close()
 
     return [
         Document(
             page_content=extracted_text,
-            metadata={"source": str(file_path)}
+            metadata={
+                "source": str(file_path)
+            }
         )
     ]
